02de73bacfa1b74c1c9b3d2dd63fe56810cf97c2
Tim Shedor <tim@wearefine.com>
2016-01-18 13:04:13 -0800

    karma missing pattern param

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -12,6 +12,7 @@ module.exports = function (config) {
       'spec/*.js',
       '../fryr.js',
       {
+        pattern: '../*.js',
         served: true,
         included: true
       }
--- test/local.karma.conf.js
+++ test/local.karma.conf.js
@@ -13,6 +13,7 @@ module.exports = function (config) {
       '../fryr.js',
 
       {
+        pattern: '../*.js',
         watched: true,
         served: true,
         included: false
